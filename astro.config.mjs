814d0b343808aa356b7a2db1ddcc88f598c0c155
Nathan Viaud <79544144+NathanViaud@users.noreply.github.com>
2025-01-14 18:11:46 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
     site: "https://NathanViaud.github.io",
-    base: "pokedex",
+    base: "my-pokedex",
     integrations: [
         starlight({
             title: "My Pokedex",
